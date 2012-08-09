1e161fb2d4f44602afd4e39e23e826db96373457
tpdorsey <terrend@mishu.com>
2012-08-09 18:34:14 -0400

    Removed archive folders code. Added single-page archive code.

--- controlPanel/classes/StaticDimension.class.php
+++ controlPanel/classes/StaticDimension.class.php
@@ -109,7 +109,7 @@
 			
 			$success = file_put_contents("../$pagePath", $page);
 			
-			$this->buildArchive($apath);
+			// $this->buildArchive($apath);
 			
 			return $pagePath;
 		}
@@ -237,7 +237,7 @@
 				unlink('../'.$pagePath);
 			}
 			
-			$this->buildArchive(substr($pagePath, 0, 10));
+			// $this->buildArchive(substr($pagePath, 0, 10));
 		}
 		
 		public function deletePage($filename)
@@ -419,28 +419,28 @@
 			return $output;
 		}
 		
-		private function buildArchivePageFromTemplate($content)
-		{
-			if(file_exists('../templates/'.$this->settings['currentTemplate'].'/_archive.html'))
-			{
-				$template = file_get_contents('../templates/'.$this->settings['currentTemplate'].'/_archive.html');
-			}
-			else if(file_exists('../templates/'.$this->settings['currentTemplate'].'/_article.html'))
-			{
-				$template = file_get_contents('../templates/'.$this->settings['currentTemplate'].'/_article.html');
-			}
-			else
-			{
-				$template = file_get_contents('../templates/'.$this->settings['currentTemplate'].'/_home.html');
-			}
+		// private function buildArchivePageFromTemplate($content)
+		// {
+		// 	if(file_exists('../templates/'.$this->settings['currentTemplate'].'/_archive.html'))
+		// 	{
+		// 		$template = file_get_contents('../templates/'.$this->settings['currentTemplate'].'/_archive.html');
+		// 	}
+		// 	else if(file_exists('../templates/'.$this->settings['currentTemplate'].'/_article.html'))
+		// 	{
+		// 		$template = file_get_contents('../templates/'.$this->settings['currentTemplate'].'/_article.html');
+		// 	}
+		// 	else
+		// 	{
+		// 		$template = file_get_contents('../templates/'.$this->settings['currentTemplate'].'/_home.html');
+		// 	}
 			
-			$output = str_replace('$PAGE_CONTENT', $content, $template);
+		// 	$output = str_replace('$PAGE_CONTENT', $content, $template);
 			
-			//TODO: needs to be more descriptive.
-			$output = str_replace('$PAGE_TITLE', 'archive', $output);
+		// 	//TODO: needs to be more descriptive.
+		// 	$output = str_replace('$PAGE_TITLE', 'archive', $output);
 			
-			return $output;
-		}
+		// 	return $output;
+		// }
 		
 		private function makeArticle($text, $title, $link, $t)
 		{
@@ -598,122 +598,122 @@
 			}
 		}
 		
-		private function buildArchive($path)
-		{
-			$months = array(1=>"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+		// private function buildArchive($path)
+		// {
+		// 	$months = array(1=>"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 		
-			$files = scandir("../$path");
+		// 	$files = scandir("../$path");
 			
-			$output = '<h1>Archive: '.$path.'</h1><nav><ul>';
-			$empty = true;
-			foreach($files as $f)
-			{
-				if($f != '..' && $f != '.' && $f != 'index.html')
-				{
-					$t = $this->getArticleTextFilenameFromArticleURL("$path/$f");
-					$a = $this->getArticleElements($t);
-					$output .= '<li><a href="'.$f.'">'.$a['title'].'</a></li>';
-					$empty = false;
-				}
-			}
-			$output .= '</ul></nav>';
+		// 	$output = '<h1>Archive: '.$path.'</h1><nav><ul>';
+		// 	$empty = true;
+		// 	foreach($files as $f)
+		// 	{
+		// 		if($f != '..' && $f != '.' && $f != 'index.html')
+		// 		{
+		// 			$t = $this->getArticleTextFilenameFromArticleURL("$path/$f");
+		// 			$a = $this->getArticleElements($t);
+		// 			$output .= '<li><a href="'.$f.'">'.$a['title'].'</a></li>';
+		// 			$empty = false;
+		// 		}
+		// 	}
+		// 	$output .= '</ul></nav>';
 			
-			if($empty)
-			{
-				//no articles in this folder any more, delete it
-				if(file_exists("../$path/index.html"))
-				{
-					unlink("../$path/index.html");
-				}
-				rmdir("../$path");
-			}
-			else
-			{
-				$page = $this->buildArchivePageFromTemplate($output);
+		// 	if($empty)
+		// 	{
+		// 		//no articles in this folder any more, delete it
+		// 		if(file_exists("../$path/index.html"))
+		// 		{
+		// 			unlink("../$path/index.html");
+		// 		}
+		// 		rmdir("../$path");
+		// 	}
+		// 	else
+		// 	{
+		// 		$page = $this->buildArchivePageFromTemplate($output);
 			
-				$success = file_put_contents("../$path/index.html", $page);
-			}
+		// 		$success = file_put_contents("../$path/index.html", $page);
+		// 	}
 			
-			$path = substr($path, 0, 7);
+		// 	$path = substr($path, 0, 7);
 			
-			$files = scandir("../$path");
+		// 	$files = scandir("../$path");
 			
-			$output = '<h1>Archive: '.$path.'</h1><nav><ul>';
-			$empty = true;
-			foreach($files as $f)
-			{
-				if($f != '..' && $f != '.' && $f != 'index.html')
-				{
-					$day = gmdate('l, d', strtotime("$path/$f 12:00:00"));
-					$output .= '<li><a href="'.$f.'">'.$day.'</a></li>';
-					$empty = false;
-				}
-			}
-			$output .= '</ul></nav>';
+		// 	$output = '<h1>Archive: '.$path.'</h1><nav><ul>';
+		// 	$empty = true;
+		// 	foreach($files as $f)
+		// 	{
+		// 		if($f != '..' && $f != '.' && $f != 'index.html')
+		// 		{
+		// 			$day = gmdate('l, d', strtotime("$path/$f 12:00:00"));
+		// 			$output .= '<li><a href="'.$f.'">'.$day.'</a></li>';
+		// 			$empty = false;
+		// 		}
+		// 	}
+		// 	$output .= '</ul></nav>';
 			
-			if($empty)
-			{
-				//no articles in this folder any more, delete it
-				if(file_exists("../$path/index.html"))
-				{
-					unlink("../$path/index.html");
-				}
-				rmdir("../$path");
-			}
-			else
-			{
-				$page = $this->buildArchivePageFromTemplate($output);
+		// 	if($empty)
+		// 	{
+		// 		//no articles in this folder any more, delete it
+		// 		if(file_exists("../$path/index.html"))
+		// 		{
+		// 			unlink("../$path/index.html");
+		// 		}
+		// 		rmdir("../$path");
+		// 	}
+		// 	else
+		// 	{
+		// 		$page = $this->buildArchivePageFromTemplate($output);
 			
-				$success = file_put_contents("../$path/index.html", $page);
-			}
+		// 		$success = file_put_contents("../$path/index.html", $page);
+		// 	}
 			
-			$path = substr($path, 0, 4);
+		// 	$path = substr($path, 0, 4);
 			
-			$files = scandir("../$path");
+		// 	$files = scandir("../$path");
 			
-			$output = '<h1>Archive: '.$path.'</h1><nav><ul>';
-			$empty = true;
-			foreach($files as $f)
-			{
-				if($f != '..' && $f != '.' && $f != 'index.html')
-				{
-					$output .= '<li><a href="'.$f.'">'.$months[intval($f)].'</a></li>';
-					$empty = false;
-				}
-			}
-			$output .= '</ul></nav>';
+		// 	$output = '<h1>Archive: '.$path.'</h1><nav><ul>';
+		// 	$empty = true;
+		// 	foreach($files as $f)
+		// 	{
+		// 		if($f != '..' && $f != '.' && $f != 'index.html')
+		// 		{
+		// 			$output .= '<li><a href="'.$f.'">'.$months[intval($f)].'</a></li>';
+		// 			$empty = false;
+		// 		}
+		// 	}
+		// 	$output .= '</ul></nav>';
 			
-			if($empty)
-			{
-				//no articles in this folder any more, delete it
-				if(file_exists("../$path/index.html"))
-				{
-					unlink("../$path/index.html");
-				}
-				rmdir("../$path");
-			}
-			else
-			{
-				$page = $this->buildArchivePageFromTemplate($output);
+		// 	if($empty)
+		// 	{
+		// 		//no articles in this folder any more, delete it
+		// 		if(file_exists("../$path/index.html"))
+		// 		{
+		// 			unlink("../$path/index.html");
+		// 		}
+		// 		rmdir("../$path");
+		// 	}
+		// 	else
+		// 	{
+		// 		$page = $this->buildArchivePageFromTemplate($output);
 			
-				$success = file_put_contents("../$path/index.html", $page);
-			}
+		// 		$success = file_put_contents("../$path/index.html", $page);
+		// 	}
 			
-			$files = array_reverse(scandir(".."));
-			$output = '<h1>Archive</h1><nav><ul>';
-			foreach($files as $f)
-			{
-				$f_ = substr($f, 0, 2);
-				if($f_ == '19' || $f_ == '20')
-				{
-					$output .= '<li><a href="'.$f.'">'.$f.'</a></li>';
-				}
-			}
-			$output .= '</ul></nav>';
-			$page = $this->buildArchivePageFromTemplate($output);
-			$success = file_put_contents("../archive.html", $page);
+		// 	$files = array_reverse(scandir(".."));
+		// 	$output = '<h1>Archive</h1><nav><ul>';
+		// 	foreach($files as $f)
+		// 	{
+		// 		$f_ = substr($f, 0, 2);
+		// 		if($f_ == '19' || $f_ == '20')
+		// 		{
+		// 			$output .= '<li><a href="'.$f.'">'.$f.'</a></li>';
+		// 		}
+		// 	}
+		// 	$output .= '</ul></nav>';
+		// 	$page = $this->buildArchivePageFromTemplate($output);
+		// 	$success = file_put_contents("../archive.html", $page);
 			
-		}
+		// }
 		
 		private function rebuildArticlePath($path)
 		{
@@ -743,35 +743,64 @@
 		
 		private function rebuildArchive()
 		{
-			//TODO - need to check for empty directories (from deleted articles)
-			$files = array_reverse(scandir(".."));
-			foreach($files as $f)
-			{
-				$f_ = substr($f, 0, 2);
-				if($f_ == '19' || $f_ == '20')
-				{
-					$files2 = array_reverse(scandir("../$f"));
+			// //TODO - need to check for empty directories (from deleted articles)
+			// $files = array_reverse(scandir(".."));
+			// foreach($files as $f)
+			// {
+			// 	$f_ = substr($f, 0, 2);
+			// 	if($f_ == '19' || $f_ == '20')
+			// 	{
+			// 		$files2 = array_reverse(scandir("../$f"));
 					
-					foreach($files2 as $f2)
-					{
+			// 		foreach($files2 as $f2)
+			// 		{
 						
-						if($f2 != '..' && $f2 != '.' && $f2 != 'index.html')
-						{
+			// 			if($f2 != '..' && $f2 != '.' && $f2 != 'index.html')
+			// 			{
 							
-							$files3 = array_reverse(scandir("../$f/$f2"));
+			// 				$files3 = array_reverse(scandir("../$f/$f2"));
 					
-							foreach($files3 as $f3)
-							{
+			// 				foreach($files3 as $f3)
+			// 				{
 						
-								if($f3 != '..' && $f3 != '.' && $f3 != 'index.html')
-								{
-									$this->buildArchive("$f/$f2/$f3");
-								}
-							}
-						}
-					}
+			// 					if($f3 != '..' && $f3 != '.' && $f3 != 'index.html')
+			// 					{
+			// 						$this->buildArchive("$f/$f2/$f3");
+			// 					}
+			// 				}
+			// 			}
+			// 		}
+			// 	}
+			// }
+
+			// Create single-page archive of all files in reverse order of creation
+			$files = array_reverse(scandir('../articles/'));
+			
+			for($i=0; $i<count($files)-1; $i++)
+			{
+				if ($files[$i] != '.' && $files[$i] != '..')
+				{
+					$content = Markdown(file_get_contents('../articles/'.$files[$i]));
+					
+					$lurl = $this->buildURLFromFilename($files[$i]);
+
+					$e = $this->getArticleElements($files[$i]);
+					
+					if ($e['link'] == '') {$url = $this->settings['siteRoot'].$lurl;}
+					else {$url = $e['link'];}
+					
+					$title = '<a href='.$url.'><h3>'.$e['title'].'</h3></a>';
+					$byline = $e['byline'];
+
+					$output .= $title.$byline."\n\n";
 				}
 			}
+			
+			$template = file_get_contents('../templates/'.$this->settings['currentTemplate'].'/_archive.html');
+			
+			$output = str_replace('$PAGE_CONTENT', $output, $template);
+			
+			$success = file_put_contents('../archive.html', $output);
 		}
 		
 		public function stripPath($path)
